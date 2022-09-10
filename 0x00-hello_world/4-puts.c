5e49a4cfe33065f818a4980fe6d6ef1637178f47
Chidera2468 <chidera2468@gmail.com>
2022-09-10 14:46:21 -0700

    for

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-
-/*
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+/**
+ *   main-Entry point*
+ *   description:Programming is like building a multilingual puzzle*
+ * Return:Always 0 (success)
+ */
 int main(void)
-{
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
-}
+{char Mylin[] = "\"Programming is like building a multilingual puzzle";													puts(Mylin);
+		 return (0); }
