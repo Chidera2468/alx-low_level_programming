d9ffe1b0402a63d1f6a4d28093888599e139940b
Chidera2468 <chidera2468@gmail.com>
2022-09-10 16:16:28 -0700

    follower

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#include<stdio.h>
-/**																			 * main-Entry point*																	* description:Programming is like building a multilingual puzzle*										       * Return:Always O (success)															      */																		   int main(void)																	  {char Mylin[] = "\" Programming is like building a multilingual puzzle";																        puts(Mylin);																	      puts(Mylin);																	    return (O); }						
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
