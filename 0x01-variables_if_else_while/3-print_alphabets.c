fd7b4feb3f1634176a142a00edf27a1836801c14
Chidera2468 <chidera2468@gmail.com>
2022-09-12 14:11:02 -0700

    dog

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main-main entry*
+ * Description:Both lower and upper case*
+ * Return:Always 0 (success)
+ */
+int main(void)
+{int x = 65;
+int y = 97;
+while (y >= 97 && y <= 122)
+{putchar(y);
+	y++; }
+	while (x >= 65 && x <= 90)
+{putchar(x);
+	x++; }
+	putchar('\n');
+	return (0); }
