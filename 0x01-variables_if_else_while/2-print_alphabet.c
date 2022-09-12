01259dcffe49e4fa5b4b5099785da92430e7fea2
Chidera2468 <chidera2468@gmail.com>
2022-09-12 13:42:02 -0700

    foot

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main-Entry function*
+ * Description:prints the alphabet in lowercase, followed by a new line.*
+ * Return:Always 0 (success)
+ */
+int main(void)
+{
+	int ch;
+	for
+		(ch = 'a';
+		 ch <= 'z';
+		 ch++)
+			putchar(ch);
+	putchar('\n');
+	return (0);
+}
