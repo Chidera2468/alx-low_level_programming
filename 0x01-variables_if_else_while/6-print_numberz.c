3fb54b3fa4f63aa144fdd703b98f55d869303462
Chidera2468 <chidera2468@gmail.com>
2022-09-12 16:05:22 -0700

    hodre

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main-entry point*
+ * Description: print 0 to 10 in new line*
+ * Return:Always 0 (success)
+ */
+int main(void)
+{ int ch;
+	for (ch = '0'; ch <= '9'; ch++)
+	{putchar(ch); }
+	putchar('\n');
+	return (0); }
