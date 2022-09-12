ad752c4c3427cbee69ed2c38c223861d8b3af9a8
Chidera2468 <chidera2468@gmail.com>
2022-09-12 13:24:34 -0700

    got

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,11 +2,11 @@
 #include<stdio.h>
 #include <time.h>
 /**
- * main-main entry*
- *description: i dont know*
- *Return:always 0 (Success)
- */
- int main(void)
+* main-main entry*
+*description: i dont know*
+*Return:always 0 (Success)
+*/
+int main(void)
 {int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
