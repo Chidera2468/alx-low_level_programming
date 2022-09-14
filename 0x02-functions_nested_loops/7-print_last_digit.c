a4df26fd87c4cf81e1b391abed2a42800b007fc0
Chidera2468 <chidera2468@gmail.com>
2022-09-13 20:31:56 -0700

    fit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,12 +2,13 @@
 /**
  * print_last_digit - prints the last digit of a number
  * @n: the int to extract the last digit from
- * Return: value of the last digit 
+ * Return:value of the last digit 
  */
 int print_last_digit(int n)
 {
 	int a;
 	if (n < 0)
+
 	n = -n;
 	a = n % 10;
 	if (a < 0)
@@ -15,4 +16,3 @@ int print_last_digit(int n)
 	_putchar(a + '0');
 	return (a);
 }
-		
