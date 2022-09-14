a1aa56a12ff5e0271110ad3518c14bd954ce6d13
Chidera2468 <chidera2468@gmail.com>
2022-09-13 17:27:49 -0700

    i

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * print_alphabet_x10- Main entry*
+ * Description: functions prototypes*
+ * Return: Always 0 (success)
+ */
+void print_alphabet_x10(void)
+{int ch;
+	int i = 48;
+
+	while (i <= 57)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+		i++;
+	}
+	return; }
