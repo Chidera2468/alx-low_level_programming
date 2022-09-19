d53a33e4e44d6515d20baf40b935b4607293ab0d
Chidera2468 <chidera2468@gmail.com>
2022-09-19 13:18:25 -0700

    srt

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,7 @@
 int _atoi(char *s)
 {
 	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
-	
+
 	while (*(s + count) != '\0')
 	{
 		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
@@ -27,7 +27,7 @@ int _atoi(char *s)
 	}
 
 	for (i = count - size; i < count; i++)
-	{	
+	{
 		oi = oi + ((*(s + i) - 48) * m);
 		m /= 10;
 	}
